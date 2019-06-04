c1870da094d99317b3199b877d6d7045eb9671db
angel-torres <alas4224@gmail.com>
2019-06-04 15:47:36 -0500

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterProfileComponent } from './character-profile/character-profile.component';
 
 const routes: Routes = [
-  {path:"", redirectTo:"home"},
+  {path:"", redirectTo:"home", pathMatch:"full"},
   {path:"home", component: HomeComponent},
-  {path:"characters", component: HomeComponent},
-  {path:"", component: HomeComponent},
+  {path:"characters", component: CharacterListComponent},
+  {path:"profile", component: CharacterProfileComponent},
 ];
 
 @NgModule({
