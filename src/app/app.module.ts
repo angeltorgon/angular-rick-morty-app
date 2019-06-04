1837b0db3baadf3af181eddeda04a1ff52f2766f
angel-torres <alas4224@gmail.com>
2019-06-04 15:43:26 -0500

    add necessary components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterCardComponent } from './character-card/character-card.component';
+import { CharacterProfileComponent } from './character-profile/character-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharacterListComponent,
+    CharacterCardComponent,
+    CharacterProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/character-profile/character-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-profile',
+  templateUrl: './character-profile.component.html',
+  styleUrls: ['./character-profile.component.css']
+})
+export class CharacterProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
